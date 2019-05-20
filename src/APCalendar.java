d6d10479d03d49d704162d5ac4c6a16b6e5f3aee
BT_1N3_07 <a>
2019-05-20 14:47:48 -0400

    mmmm

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,7 +1,36 @@
 public class APCalendar
 {
+
     private static boolean isLeapYear(int year)
     {
-        return false;
+        if(year%4==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static int numberofLeapYears(int year1, int year2)
+    {
+        int num = 0;
+        if(year1%4==0)
+        {
+
+        }
+        return num;
+    }
+    private static int firstDayOfYear(int year)
+    {
+
+    }
+    private static int dayofYear(int month, int day, int year)
+    {
+
+    }
+    public static int dayOfWeek(int month, int day, int year)
+    {
+
     }
 }
